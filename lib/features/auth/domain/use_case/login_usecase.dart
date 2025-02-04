90a9591bd8042e54b72ed113f034ff901869fc8b
aashishkar <aashishkarmacharya79@gmail.com>
2025-02-04 10:05:01 +0545

    login usecase

--- lib/features/auth/domain/use_case/login_usecase.dart
+++ lib/features/auth/domain/use_case/login_usecase.dart
@@ -1,9 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import 'package:wise_academy/app/shared_prefs/token_shared_prefs.dart';
-import 'package:wise_academy/app/usecase/usecase.dart';
-import 'package:wise_academy/core/error/failure.dart';
-import 'package:wise_academy/features/auth/domain/repository/auth_repository.dart';
+import 'package:mobileapplicationdevelopment/app/shared_prefs/token_shared_prefs.dart';
+import 'package:mobileapplicationdevelopment/app/usecase/usecase.dart';
+import 'package:mobileapplicationdevelopment/core/error/failure.dart';
+import 'package:mobileapplicationdevelopment/features/auth/domain/repository/auth_repository.dart';
+
 
 class LoginParams extends Equatable {
   final String email;
