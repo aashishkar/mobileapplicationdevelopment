6594fcce658758f33eed715a7238e2aef215a728
aashishkar <aashishkarmacharya79@gmail.com>
2025-02-04 09:58:17 +0545

    app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wise_academy/app/di/di.dart';
-import 'package:wise_academy/core/theme/app_theme.dart';
-import 'package:wise_academy/features/splash/presentation/view/splash_view.dart';
-import 'package:wise_academy/features/splash/presentation/view_model/splash_cubit.dart';
+import 'package:mobileapplicationdevelopment/app/di/di.dart';
+import 'package:mobileapplicationdevelopment/core/theme/app_theme.dart';
+import 'package:mobileapplicationdevelopment/features/splash/presentation/view/splash_view.dart';
+import 'package:mobileapplicationdevelopment/features/splash/presentation/view_model/splash_cubit.dart';
+
 
 class App extends StatelessWidget {
   const App({super.key});
